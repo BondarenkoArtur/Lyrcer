09f71da43957cdd113c0ee0cd679cc363a12a7c2
Artur Bondarenko <ua.bondarenko.artur@gmail.com>
2017-02-23 21:38:05 +0200

    Initialized microproject without unneded files

---
+++ app/src/main/java/ga/uabart/lyrcer/MainActivity.java
@@ -0,0 +1,13 @@
+package ga.uabart.lyrcer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
